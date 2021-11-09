37cb0706cecb0550ddee31061c689cf04d458cb2
thinegen <git@thinegen.de>
2021-11-09 15:48:56 +0100

    treat other baseUrls as same host in external links js

--- assets/js/external-links.js
+++ assets/js/external-links.js
@@ -1,6 +1,13 @@
+var baseURLArray = [
+    {{ range .Site.Languages }}
+        {{ $langBaseUrl := urls.Parse .Params.BaseUrl }}
+        "{{ $langBaseUrl.Host }}",
+    {{ end }}
+]
+
 for(i = 0; i < document.links.length; i++) {
     var link = document.links[i];
-    if (link.host !== location.host) {
+    if (!baseURLArray.includes(link.host)) {
         link.target = '_blank';
         link.rel = "noopener external";
         {{ if .Site.Params.markExternalLinks }}
