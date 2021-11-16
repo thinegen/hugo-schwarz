22cfd053523364e37a2212ece0c865372ec6edd9
thinegen <git@thinegen.de>
2021-11-16 15:52:00 +0100

    fix external link js

--- assets/js/external-links.js
+++ assets/js/external-links.js
@@ -1,15 +1,22 @@
 var baseURLArray = [
-    {{ range .Site.Languages }}
-        {{ $langBaseUrl := urls.Parse .Params.BaseUrl }}
+    {{ if .Site.IsMultiLingual }}
+        {{ range .Site.Languages }}
+            {{ $langBaseUrl := urls.Parse .Params.BaseURL }}
+            "{{ $langBaseUrl.Host }}",
+        {{ end }}
+    {{ else }}
+        {{ $langBaseUrl := urls.Parse .Site.BaseURL }}
         "{{ $langBaseUrl.Host }}",
     {{ end }}
-]
+];
 
 for(i = 0; i < document.links.length; i++) {
     var link = document.links[i];
     if (!baseURLArray.includes(link.host)) {
-        link.target = '_blank';
-        link.rel = "noopener external";
+        {{ if not .Site.IsServer }}
+            link.target = '_blank';
+            link.rel = "noopener external";
+        {{ end }}
         {{ if .Site.Params.markExternalLinks }}
             link.classList.add("external-link");
         {{ end }}
